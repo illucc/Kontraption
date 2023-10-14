d313a043e264270c1b8f3379065904e94b4a3af3
illucc <illucizer@gmail.com>
2023-10-14 23:02:07 +0300

    testing a thing

--- src/main/kotlin/net/illuc/kontraption/KontraptionLang.kt
+++ src/main/kotlin/net/illuc/kontraption/KontraptionLang.kt
@@ -6,6 +6,7 @@ import net.minecraft.Util;
 
 
 enum class KontraptionLang(private val key: String) : ILangEntry {
+    //h
     DESCRIPTION_ION_THRUSTER("description", "ion_thruster");
 
     constructor(type: String, path: String) : this(Util.makeDescriptionId(type, Kontraption.rl(path)))
