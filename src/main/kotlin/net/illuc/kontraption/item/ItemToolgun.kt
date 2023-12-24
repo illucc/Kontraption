bcca2f4fa0ee315dd6e2addbbeece6b411e53b67
illucc <illucizer@gmail.com>
2023-12-24 22:57:25 +0200

    fix: moare fixers

--- src/main/kotlin/net/illuc/kontraption/item/ItemToolgun.kt
+++ src/main/kotlin/net/illuc/kontraption/item/ItemToolgun.kt
@@ -20,20 +20,24 @@ import mekanism.common.util.MekanismUtils
 import mekanism.common.util.StorageUtils
 import net.illuc.kontraption.KontraptionLang
 import net.illuc.kontraption.config.KontraptionConfigs
-import net.illuc.kontraption.util.KontraptionVSUtils
+import net.illuc.kontraption.util.*
 import net.illuc.kontraption.util.KontraptionVSUtils.createNewShipWithBlocks
 import net.minecraft.Util
 import net.minecraft.core.BlockPos
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.TextComponent
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
+import net.minecraft.world.InteractionResultHolder
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Rarity
 import net.minecraft.world.item.context.UseOnContext
+import net.minecraft.world.level.ClipContext
 import net.minecraft.world.level.Level
+import org.joml.Vector3d
 import org.valkyrienskies.core.util.datastructures.DenseBlockPosSet
 import javax.annotation.Nonnull
 import kotlin.math.max
@@ -47,57 +51,72 @@ class ItemToolgun(properties: Properties) : ItemEnergized(KontraptionConfigs.kon
     var firstPosition: BlockPos? = null
     var secondPosition: BlockPos? = null
 
-    @Nonnull
-    override fun useOn(context: UseOnContext): InteractionResult {
-        val player = context.player
-        val world: Level = context.level
-        context.player!!.playSound(MekanismSounds.BEEP.get(), 1F, 1F)
-        if (!world.isClientSide && player != null) {
-            val pos = context.clickedPos
-            if (!player.isCreative) {
-                val energyPerUse = KontraptionConfigs.kontraption.toolgunActionConsumption.get()
-                val energyContainer = StorageUtils.getEnergyContainer(context.itemInHand, 0)
-                if (energyContainer == null || energyContainer.extract(energyPerUse, Action.SIMULATE, AutomationType.MANUAL).smallerThan(energyPerUse)) {
-                    return InteractionResult.FAIL
-                }
-                energyContainer.extract(energyPerUse, Action.EXECUTE, AutomationType.MANUAL)
+    override fun use(level: Level, player: Player, interactionHand: InteractionHand): InteractionResultHolder<ItemStack> {
+        val clipResult = level.clip(
+                ClipContext(
+                        (Vector3d(player.eyePosition.toJOML()).toMinecraft()),
+                        (player.eyePosition.toJOML()
+                                .add(0.5, 0.5, 0.5)
+                                .add(Vector3d(player.lookAngle.toJOML()).mul(10.0)) //distance
+                                ).toMinecraft(),
+                        ClipContext.Block.COLLIDER,
+                        ClipContext.Fluid.NONE,
+                        null
+                )
+        )
+        //player.sendMessage(TextComponent(" ${clipResult.blockPos}"), Util.NIL_UUID)
+        player.playSound(MekanismSounds.BEEP.get(), 1F, 1F)
+
+        val pos = clipResult.blockPos
+        if (!player.isCreative) {
+            val energyPerUse = KontraptionConfigs.kontraption.toolgunActionConsumption.get()
+            val energyContainer = StorageUtils.getEnergyContainer(player.getItemInHand(interactionHand), 0)
+            if (energyContainer == null || energyContainer.extract(energyPerUse, Action.SIMULATE, AutomationType.MANUAL).smallerThan(energyPerUse)) {
+                return super.use(level, player, interactionHand)
             }
-            if (getMode(context.itemInHand) == ToolgunMode.ASSEMBLE){
-                makeSelection(pos, context)
-            } else if (getMode(context.itemInHand) == ToolgunMode.MOVE){
-                context.player?.sendMessage(TextComponent("Work in progress :P"), Util.NIL_UUID)
-            } else if (getMode(context.itemInHand) == ToolgunMode.LOCK){
-                context.player?.sendMessage(TextComponent("Work in progress :P"), Util.NIL_UUID)
-            } else if (getMode(context.itemInHand) == ToolgunMode.PUSH){
-                context.player?.sendMessage(TextComponent("Work in progress :P"), Util.NIL_UUID)
-            } else if (getMode(context.itemInHand) == ToolgunMode.ROTATE){
-                context.player?.sendMessage(TextComponent("Work in progress :P"), Util.NIL_UUID)
-            }
-            return InteractionResult.CONSUME
+            energyContainer.extract(energyPerUse, Action.EXECUTE, AutomationType.MANUAL)
         }
-        return InteractionResult.PASS
+        if (getMode(player.getItemInHand(interactionHand)) == ToolgunMode.ASSEMBLE){
+            makeSelection(level, player, interactionHand, pos)
+        } else if (getMode(player.getItemInHand(interactionHand)) == ToolgunMode.MOVE){
+            player.sendMessage(TextComponent("Work in progress :P"), Util.NIL_UUID)
+        } else if (getMode(player.getItemInHand(interactionHand)) == ToolgunMode.LOCK){
+            player.sendMessage(TextComponent("Work in progress :P"), Util.NIL_UUID)
+        } else if (getMode(player.getItemInHand(interactionHand)) == ToolgunMode.PUSH){
+            player.sendMessage(TextComponent("Work in progress :P"), Util.NIL_UUID)
+        } else if (getMode(player.getItemInHand(interactionHand)) == ToolgunMode.ROTATE){
+            player.sendMessage(TextComponent("Work in progress :P"), Util.NIL_UUID)
+        }
+
+
+        return super.use(level, player, interactionHand)
     }
 
-    fun makeSelection(pos: BlockPos, context: UseOnContext){
+    fun makeSelection(level: Level, player: Player, interactionHand: InteractionHand, pos: BlockPos){
 
-        if (!context.level.isClientSide) {
-        if (firstPosition == null){
-            if (KontraptionVSUtils.getShipObjectManagingPos(context.level, pos) == null) {
+        if (!level.isClientSide) {
+            println(level.getBlockState(pos))
+            if (player.isShiftKeyDown and (level.getBlockState(pos).isAir)){
+                firstPosition = null
+                secondPosition = null
+                player.sendMessage(TextComponent("Selection reset"), Util.NIL_UUID)
+            }
+        else if (firstPosition == null){
+            if (KontraptionVSUtils.getShipObjectManagingPos(level, pos) == null) {
                 firstPosition = pos
                 Mekanism.logger.info("first pos: $firstPosition")
-                context.player?.sendMessage(TextComponent("First pos selected"), Util.NIL_UUID)
+                player.sendMessage(TextComponent("First pos selected"), Util.NIL_UUID)
             } else{
-                context.player?.sendMessage(TextComponent("Selected position is on a ship!"), Util.NIL_UUID)
+                player.sendMessage(TextComponent("Selected position is on a ship!"), Util.NIL_UUID)
             }
         }else if(secondPosition == null){
-            if (KontraptionVSUtils.getShipObjectManagingPos(context.level, pos) == null) {
+            if (KontraptionVSUtils.getShipObjectManagingPos(level, pos) == null) {
                 secondPosition = pos
                 Mekanism.logger.info("second pos: $secondPosition")
-                context.player?.sendMessage(TextComponent("Second pos selected"), Util.NIL_UUID)
+                player.sendMessage(TextComponent("Second pos selected"), Util.NIL_UUID)
             } else{
-                context.player?.sendMessage(TextComponent("Selected position is on a ship!"), Util.NIL_UUID)
+                player.sendMessage(TextComponent("Selected position is on a ship!"), Util.NIL_UUID)
             }
-
         }else{
             Mekanism.logger.info("now we do the assembly stuff")
             val set = DenseBlockPosSet()
@@ -107,23 +126,27 @@ class ItemToolgun(properties: Properties) : ItemEnergized(KontraptionConfigs.kon
             for (x in min(firstPosition!!.x, secondPosition!!.x)..max(firstPosition!!.x, secondPosition!!.x)) {
                 for (y in min(firstPosition!!.y, secondPosition!!.y)..max(firstPosition!!.y, secondPosition!!.y)) {
                     for (z in min(firstPosition!!.z, secondPosition!!.z)..max(firstPosition!!.z, secondPosition!!.z)) {
-                        set.add(x, y, z)
+                        if(!level.getBlockState(BlockPos(x, y, z)).isAir){
+                            set.add(x, y, z)
+                        }
                     }
                 }
             }
 
             val energyPerUse = KontraptionConfigs.kontraption.toolgunAssembleConsumption.get().multiply(set.size.toDouble())
-            val energyContainer = StorageUtils.getEnergyContainer(context.itemInHand, 0)
+            val energyContainer = StorageUtils.getEnergyContainer(player.getItemInHand(interactionHand), 0)
             if (energyContainer == null || energyContainer.extract(energyPerUse, Action.SIMULATE, AutomationType.MANUAL).smallerThan(energyPerUse)) {
                 if (energyContainer != null) {
                     Mekanism.logger.info("Assembly failed! Not enough energy, $energyPerUse needed but had ${energyContainer.energy}")
-                    context.player?.sendMessage(TextComponent("Assembly failed! Not enough energy, $energyPerUse needed but had ${energyContainer.energy}"), Util.NIL_UUID)
+                    player.sendMessage(TextComponent("Assembly failed! Not enough energy, $energyPerUse needed but had ${energyContainer.energy}"), Util.NIL_UUID)
                 }
             }else{
-                energyContainer.extract(energyPerUse, Action.EXECUTE, AutomationType.MANUAL)
-                createNewShipWithBlocks(pos, set, (context.level as ServerLevel))
-                context.player!!.playSound(MekanismSounds.BEEP.get(), 1F, 2F)
-                context.player?.sendMessage(TextComponent("Assembled!"), Util.NIL_UUID)
+                if (!set.isEmpty()){
+                    energyContainer.extract(energyPerUse, Action.EXECUTE, AutomationType.MANUAL)
+                    createNewShipWithBlocks(pos, set, (level as ServerLevel))
+                    player.playSound(MekanismSounds.BEEP.get(), 1F, 2F)
+                    player.sendMessage(TextComponent("Assembled!"), Util.NIL_UUID)
+                }
             }
             firstPosition = null
             secondPosition = null
